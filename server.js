1f2f231d42d9aed207ab0e3df5816a2be2b135d4
andrewrasz <34610575+andrewrasz@users.noreply.github.com>
2023-10-23 11:12:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const port = 3000; // You can change the port as needed
 
 app.use(express.json());
 
